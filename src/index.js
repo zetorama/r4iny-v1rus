be878d26cc6f44cf3a005f0b69d9728e780959af
Pavel Zet <zetorama@gmail.com>
2019-12-08 02:16:14 -0800

    add error boundary to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { ErrorBoundary } from 'react-error-boundary'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ErrorReport = ({ componentStack, error }) => (
+  <div className='Error'>
+    <p><strong>Oops! An error occured!</strong></p>
+    <p>Here’s what we know…</p>
+    <p><strong>Error:</strong> {error.toString()}</p>
+    <p>
+      <strong>Stacktrace:</strong>
+      <pre>{componentStack}</pre>
+    </p>
+  </div>
+)
+
+const Index = () => (
+  <ErrorBoundary FallbackComponent={ErrorReport}>
+    <App />
+  </ErrorBoundary>
+)
+
+ReactDOM.render(<Index />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
